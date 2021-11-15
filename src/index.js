a1562ad64fd4ea529628042de6578c0e828033cf
Hudson Holanda <hudsonholanda55@gmail.com>
2021-11-15 11:27:48 -0300

    feat: get users with query params of ordenation

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 const http = require("http");
-
+const { URL } = require("url");
 const routes = require("./routes");
 
 const server = http.createServer((request, response) => {
-  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+  const parsedUrl = new URL(`http://localhost:3000${request.url}`);
+  console.log(
+    `Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`
+  );
   const route = routes.find(
     (routObj) =>
-      routObj.endpoint === request.url && routObj.method === request.method
+      routObj.endpoint === parsedUrl.pathname &&
+      routObj.method === request.method
   );
   if (route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams);
     route.handler(request, response);
   } else {
     response.writeHead(404, { "Content-Type": "text/html" });
-    response.end(`Cannot ${request.method} ${request.url}`);
+    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
   }
 });
 
