a08130ded80f3f77ae1b4195cdc2e0def9e759fe
Hudson Holanda <hudsonholanda55@gmail.com>
2021-12-03 21:02:49 -0300

    feat: use body parser in POST and PUT requests

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const { URL } = require("url");
 const routes = require("./routes");
+const bodyParser = require('./helpers/bodyParser');
 
 const server = http.createServer((request, response) => {
   const parsedUrl = new URL(`http://localhost:3000${request.url}`);
@@ -27,7 +28,11 @@ const server = http.createServer((request, response) => {
       response.writeHead(statusCode, { "Content-Type": "application/json" });
       response.end(JSON.stringify(body));
     } 
-    route.handler(request, response);
+    if(['POST', 'PUT'].includes(request.method)) {
+      bodyParser(request, () => route.handler(request, response));
+    } else {  
+      route.handler(request, response);
+    }
   } else {
     response.writeHead(404, { "Content-Type": "text/html" });
     response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
